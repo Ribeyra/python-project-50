201a8eda2512a8ecdd322da8647892e92c65fff6
ribeyra <ribeyra@gmail.com>
2024-01-19 19:50:23 +0300

    complete step three

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Ribeyra/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ribeyra/python-project-50/actions)
+
+**Comparison of flat files (JSON)**  
+[![asciicast](https://asciinema.org/a/6uGQjczkHGk2xdCxMUpFXxHGr.svg)](https://asciinema.org/a/6uGQjczkHGk2xdCxMUpFXxHGr)
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,11 +1,46 @@
 import argparse
+import json
 
-parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.'
-)
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format', help='set format of output')
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
+
+
+def generate_diff(first_file, second_file):
+    file1 = json.load(open(first_file), parse_int=str)
+    y = json.load(open(second_file), parse_int=str)
+
+    def replace_bool_to_str(value: bool) -> str:
+        if value is True:
+            return 'true'
+        elif value is False:
+            return 'false'
+        return value
+
+    lines = []
+    for el in sorted(set(file1) | set(y)):
+        if el in file1 and el in y:
+            if file1[el] != y[el]:
+                lines.append(f'  - {el}: {replace_bool_to_str(file1[el])}\n'
+                             f'  + {el}: {replace_bool_to_str(y[el])}')
+            else:
+                lines.append(f'    {el}: {replace_bool_to_str(file1[el])}')
+        elif el in file1:
+            lines.append(f'  - {el}: {replace_bool_to_str(file1[el])}')
+        elif el in y:
+            lines.append(f'  + {el}: {replace_bool_to_str(y[el])}')
+
+    result = '{\n' + '\n'.join(lines) + '\n}'
+    return result
+
+
+if __name__ == '__main__':
+    main()
