c0459466c773a361befa4a161f0087f97c1d56d2
ribeyra <ribeyra@gmail.com>
2024-01-31 17:55:41 +0300

    add more tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,101 @@
-from gendiff.gendiff import generate_diff
+from gendiff.gendiff import replace_bool_or_None_to_str, add_atribut, \
+    compar_values, differ, generate_diff
+
+
+def test_replace_bool_or_None_to_str():
+    assert replace_bool_or_None_to_str(True) == 'true'
+    assert replace_bool_or_None_to_str(False) == 'false'
+    assert replace_bool_or_None_to_str(None) == 'null'
+    assert replace_bool_or_None_to_str('some text') == 'some text'
+    assert replace_bool_or_None_to_str(12) == 12
+
+
+def test_add_atribut():
+    data = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+    exp_res = {
+        'key1': [' ', 'value1'],
+        'key2': [' ', 'value2'],
+        'key3': [' ', 'value3']
+    }
+    add_atribut(data)
+    assert data == exp_res
+
+    data2 = {
+        'key1': 'value1',
+        'key2': {
+            'inner_key1': 'inner_value1',
+            'inner_key2': 'inner_value2'
+        },
+        'key3': 'value3'
+    }
+    exp_res2 = {
+        'key1': [' ', 'value1'],
+        'key2': [' ', {
+            'inner_key1': [' ', 'inner_value1'],
+            'inner_key2': [' ', 'inner_value2']
+        }],
+        'key3': [' ', 'value3']
+    }
+    add_atribut(data2)
+    assert data2 == exp_res2
+
+
+def test_differ():
+    data1 = {
+        'key1': [' ', 'value1'],
+        'key2': [' ', {
+            'inner_key1': [' ', 'inner_value1'],
+            'inner_key2': [' ', 'inner_value2']
+        }],
+        'key3': [' ', 'value3']
+    }
+    data2 = {
+        'key1': [' ', 'true'],
+        'key2': [' ', {
+            'inner_key1': [' ', 'some_value1'],
+            'inner_key3': [' ', 'inner_value2']
+        }],
+        'key3': [' ', 'value3']
+    }
+    exp_res = {
+        'key1': ['*', 'value1', 'true'],
+        'key2': [' ', {
+            'inner_key1': ['*', 'inner_value1', 'some_value1'],
+            'inner_key2': ['-', 'inner_value2'],
+            'inner_key3': ['+', 'inner_value2']
+        }],
+        'key3': [' ', 'value3']
+    }
+    assert differ(data1, data2) == exp_res
+
+
+def test_compar_values():
+
+    key = 'key'
+    data = {}
+    attributes1 = [' ', 'old_value']
+    attributes2 = [None, None]
+    compar_values(key, data, attributes1, attributes2)
+    assert data == {'key': ['-', 'old_value']}
+
+    data = {}
+    attributes1 = [None, None]
+    attributes2 = [' ', 'new_value']
+    compar_values(key, data, attributes1, attributes2)
+    assert data == {'key': ['+', 'new_value']}
+
+    data = {}
+    attributes1 = [' ', 'old_value']
+    attributes2 = [' ', 'new_value']
+    compar_values(key, data, attributes1, attributes2)
+    assert data == {'key': ['*', 'old_value', 'new_value']}
+
+    data = {}
+    attributes1 = [' ', 'old_value']
+    attributes2 = [' ', 'old_value']
+    compar_values(key, data, attributes1, attributes2)
+    assert data == {'key': [' ', 'old_value']}
+
 
 file1_json = 'tests/fixtures/file1.json'
 file2_json = 'tests/fixtures/file2.json'
